0d8f20a2b587bd1618c5fc07baa607e6968eba27
Adrian Zinko <adrian.zinko@boldare.com>
2022-07-03 17:23:31 +0200

    feat(): handle auth form panels

--- src/app/modules/auth/auth.module.ts
+++ src/app/modules/auth/auth.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoginFormComponent } from './components/login-form/login-form.component';
+import { SharedModule } from '../shared/shared.module';
 
 
 
@@ -9,7 +10,8 @@ import { LoginFormComponent } from './components/login-form/login-form.component
     LoginFormComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    SharedModule
   ],
   exports: [
     LoginFormComponent
--- src/app/modules/shared/shared.module.ts
+++ src/app/modules/shared/shared.module.ts
@@ -1,18 +1,24 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavComponent } from './components/nav/nav.component';
+import { TabsContainerComponent } from './components/tabs-container/tabs-container.component';
+import { TabComponent } from './components/tab/tab.component';
 
 
 
 @NgModule({
   declarations: [
-    NavComponent
+    NavComponent,
+    TabsContainerComponent,
+    TabComponent
   ],
   imports: [
     CommonModule,
   ],
   exports: [
-    NavComponent
+    NavComponent,
+    TabsContainerComponent,
+    TabComponent
   ]
 })
 export class SharedModule { }
