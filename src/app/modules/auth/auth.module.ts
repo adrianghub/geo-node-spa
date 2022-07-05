b434b729cfdac10ee3fc7074ba2c8cbea2864832
Adrian Zinko <adrian.zinko@boldare.com>
2022-07-05 10:46:43 +0200

    refactor(auth-form): rename component

--- src/app/modules/auth/auth.module.ts
+++ src/app/modules/auth/auth.module.ts
@@ -1,20 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { LoginFormComponent } from './components/login-form/login-form.component';
+import { AuthFormComponent } from './components/auth-form/auth-form.component';
 import { SharedModule } from '../shared/shared.module';
 
 
 
 @NgModule({
   declarations: [
-    LoginFormComponent
+    AuthFormComponent
   ],
   imports: [
     CommonModule,
     SharedModule
   ],
   exports: [
-    LoginFormComponent
+    AuthFormComponent
   ]
 })
 export class AuthModule { }
